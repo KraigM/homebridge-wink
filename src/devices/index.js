dd437f90286664b5c5f28bd387c71da68d06ec4a
Simon Bartlett <simon@sibartlett.com>
2017-05-12 01:21:36 -0400

    Improved error stack traces using source maps

--- src/devices/index.js
+++ src/devices/index.js
@@ -4,6 +4,7 @@ export default options => {
   return requireAll({
     dirname: __dirname,
     filter: filename =>
+      filename.indexOf(".js.map") === -1 &&
       filename !== "index.js" &&
       filename !== "_shared.js" &&
       filename.split(".js")[0],
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import sourceMapSupport from "source-map-support";
 import WinkPlatform, { pluginName, platformName } from "./WinkPlatform";
+
+sourceMapSupport.install();
 
 export default homebridge => {
   homebridge.registerPlatform(pluginName, platformName, WinkPlatform, true);
